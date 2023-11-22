a177929ffd8f34c470a0546b29449ea7187dfbae
Olising <owsinger@outlook.com>
2023-11-22 01:46:14 +0000

    Created resources for font, colour etc. storage and displayed a value on the screen.

---
+++ Resources/Fonts/fonts.py
@@ -0,0 +1,7 @@
+#fonts
+
+import pygame as py
+
+py.init()
+
+cash_font = py.font.Font(None, 32)
---
+++ base.py
@@ -0,0 +1,31 @@
+#Base
+#
+#VERSION
+#Pre-Alpha0.0_1
+#
+
+import pygame as py
+from Resources.Fonts.fonts import *
+
+
+text = cash_font.render(str(23), True, (0, 0, 0))
+#initialising pygame
+py.init()
+
+#-----------
+
+screen = py.display.set_mode((800, 600))
+screen.fill((255, 255, 255))
+screen.blit(text, (20, 20))
+py.display.flip()
+print("you got to here")
+
+running = True
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    py.display.flip()
+
+py.quit()
