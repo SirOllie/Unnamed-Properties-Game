caaed6a25e14ab1e1d2604ffa33e4d26e9c88b3c
Olising <owsinger@outlook.com>
2023-11-22 23:38:47 +0000

    Added background to screen

--- Resources/Variables/variables.py
+++ Resources/Variables/variables.py
@@ -4,9 +4,10 @@ import pygame as py
 from Resources.Colours.colours import *
 
 ########## SCREEN ##########
-screen = py.display.set_mode((768, 432))
+screen_size = (1280, 720)
+screen = py.display.set_mode(screen_size)
 bg = py.image.load("Resources/Images/Background1.png")
-bg = py.transform.scale(bg, (768, 432))
+bg = py.transform.scale(bg, screen_size)
 ########## CASH ##########
 cash_val = 0
 cash_text_position = (20, 20)
--- base.py
+++ base.py
@@ -34,12 +34,14 @@ while running:
                 screen.blit(bg, (0, 0))
                 screen.blit(cash_text, cash_text_position)
                 py.display.flip()
+
             if event.key == py.K_LEFT:
                 cash_val = cash_val_change(cash_val, -1)
                 cash_text = cash_font.render("Cash: " + str(cash_val), True, cash_text_colour)
                 screen.blit(bg, (0, 0))
                 screen.blit(cash_text, cash_text_position)
                 py.display.flip()
+                
     py.display.flip()
 
 py.quit()
